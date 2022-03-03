f2d2e6f8cace0e5e382de659edadc4e2a561aad3
TImothy Beck <Timothy_Beck@student.uml.edu>
2022-03-03 15:13:26 -0500

    More Changes
    Small Changes

--- query1.php
+++ query1.php
@@ -11,4 +11,5 @@ $result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_e
 $query2 = "INSERT INTO customer VALUES ('$Email', '$Username', '$Password' , 0)";
 $result2 = mysqli_query($myconnection, $query2) or die ('Query failed: ' . mysql_error());
 mysqli_close($myconnection);
+header("Location: home.html");
 ?>
