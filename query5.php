c77f5a785aed22b0daf63fe5c4c806e413f827d3
jackschnair <12jack812@gmail.com>
2022-03-10 20:28:23 -0500

    Update query5.php
    query works in sqlite locally, but not witht the mysql php

--- query5.php
+++ query5.php
@@ -10,9 +10,13 @@ $myconnection = mysqli_connect('localhost', 'root', '')
 
 $mydb = mysqli_select_db ($myconnection, 'bookstore') or die ('Could not select database');
 
-if (is_null($Year)) { // user didn't enter year
-    $query = "";
+if (is_null($Year) || $Year == "") { // user didn't enter year
+    echo "Blank Query";
+    //$query = "SELECT Title, Sold FROM (SELECT ISBN, count(ISBN) as Sold FROM in_order GROUP BY ISBN) io, Book WHERE Book.ISBN = io.ISBN ORDER BY Sold DESC";
+    $query = "SELECT Title FROM Book,(SELECT ISBN, MAX(Sold) FROM (SELECT ISBN, sum(quantity) as Sold FROM in_order GROUP BY ISBN)) t1 WHERE Book.ISBN = t1.ISBN";
+    //$query = "SELECT ISBN, MAX(Sold) FROM (SELECT ISBN, sum(quantity) as Sold FROM in_order GROUP BY ISBN)";
     $result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_error());
+    //echo $result;
 }
 else { // searches for best selling books of that year
     $query = "";
