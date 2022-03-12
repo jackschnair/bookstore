56eab11167f98119c3bdee3b906bf6bc13e35a2b
jackschnair <12jack812@gmail.com>
2022-03-12 13:16:35 -0500

    Update query5.php
    Query now works correctly

--- query5.php
+++ query5.php
@@ -4,36 +4,60 @@
 // if no year is given, return the best-selling book for the entire history.
 
 $Year = $_POST['year'];
-echo $Year;
+//echo $Year;
 $myconnection = mysqli_connect('localhost', 'root', '') 
     or die ('Could not connect: ' . mysql_error());
 
 $mydb = mysqli_select_db ($myconnection, 'bookstore') or die ('Could not select database');
 
 if (is_null($Year) || $Year == "") { // user didn't enter year
-    echo "Blank Query";
-    //$query = "SELECT Title, Sold FROM (SELECT ISBN, count(ISBN) as Sold FROM in_order GROUP BY ISBN) io, Book WHERE Book.ISBN = io.ISBN ORDER BY Sold DESC";
-    $query = "SELECT Title FROM Book,(SELECT ISBN, MAX(Sold) FROM (SELECT ISBN, sum(quantity) as Sold FROM in_order GROUP BY ISBN)) t1 WHERE Book.ISBN = t1.ISBN";
-    //$query = "SELECT ISBN, MAX(Sold) FROM (SELECT ISBN, sum(quantity) as Sold FROM in_order GROUP BY ISBN)";
-    $result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_error());
-    //echo $result;
+    $query = "SELECT Title 
+	FROM (SELECT ISBN, sum(quantity) as Sold FROM in_order 
+		WHERE ISBN in 
+			(SELECT ISBN FROM Book) GROUP BY ISBN) t2, Book	WHERE t2.Sold in 
+				(SELECT MAX(t1.Sold) FROM 
+					(SELECT ISBN, sum(quantity) as Sold FROM in_order WHERE ISBN in 
+						(SELECT ISBN FROM Book) GROUP BY ISBN) t1)
+		AND Book.ISBN = t2.ISBN;"; 
+	$result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_error());
+    
+	while ($row = mysqli_fetch_array ($result, MYSQLI_ASSOC)) {
+		echo "Title(s)";
+		echo '<br>';
+		echo $row["Title"];
+		echo '<br>';
+	}
+	mysqli_free_result($result);
 }
 else { // searches for best selling books of that year
-    $query = "SELECT ISBN 
+    $query = "SELECT Title 
 	      FROM (SELECT ISBN, sum(quantity) as Sold 
 		    FROM in_order 
 		    WHERE ISBN = (SELECT ISBN 
 				  FROM Book 
-				  WHERE Date_Published LIKE '%2013%') 
-		    GROUP BY ISBN) t2 	
-	      WHERE t2.Sold = (SELECT MAX(t1.Sold) 
+				  WHERE Date_Published LIKE '%$Year%') 
+		    GROUP BY ISBN) t2, Book	
+	      WHERE t2.Sold in (SELECT MAX(t1.Sold) 
 				FROM (SELECT ISBN, sum(quantity) as Sold 
 				      FROM in_order 
-				      WHERE ISBN = (SELECT ISBN 
+				      WHERE ISBN in (SELECT ISBN 
 					            FROM Book 
-						   WHERE Date_Published LIKE '%2013%') 
-				      GROUP BY ISBN) t1)"; 
+						   WHERE Date_Published LIKE '%$Year%') 
+				      GROUP BY ISBN) t1)
+				AND Book.ISBN = t2.ISBN"; 
     $result = mysqli_query($myconnection, $query) or die ('Query failed: ' . mysql_error());
+	if (mysqli_num_rows($result) > 0) {
+		while ($row = mysqli_fetch_array ($result, MYSQLI_ASSOC)) {
+			echo "Title(s)";
+			echo '<br>';
+			echo $row["Title"];
+			echo '<br>';
+		}
+		mysqli_free_result($result);
+	}
+	else {
+		echo "No best selling book of $Year";
+	}
 }
 mysqli_close($myconnection);
 
